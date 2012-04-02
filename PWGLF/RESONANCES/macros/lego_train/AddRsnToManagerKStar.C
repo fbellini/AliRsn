05c596cf3c4102b52732b2ef87c6c1765f485457
Martin Vala <Martin.Vala@cern.ch>
2012-04-02 14:07:15 +0200

    - Set default for kStart analysis

--- PWGLF/RESONANCES/macros/lego_train/AddRsnToManagerKStar.C
+++ PWGLF/RESONANCES/macros/lego_train/AddRsnToManagerKStar.C
@@ -9,11 +9,14 @@ Bool_t AddRsnToManagerKStar(TList *listRsn) {
    // Note : for now you have to set gRsnUseMiniPackage = 0 to have mon histograms
    //    listRsn->Add(new TNamed("<Name>:mon","<CutName>:<opt>"));
 
+   // defautl PID - NSigma -> TPC=3 and TOF=3
    listRsn->Add(new TNamed("KStar","KStarNsigma"));
-   listRsn->Add(new TNamed("KStar","KStarNsigma:qualityonly"));
+   
+   // quality only
+//    listRsn->Add(new TNamed("KStar","KStarNsigma:qualityonly"));
    
    // this is TPC only
-   listRsn->Add(new TNamed("KStar","KStarNsigma:PiTPCnsig10_KTPCnsig20"));
+//    listRsn->Add(new TNamed("KStar","KStarNsigma:PiTPCnsig10_KTPCnsig20"));
 // 
 //    // this is TOF only
 //    listRsn->Add(new TNamed("KStar","KStarNsigma:PiTOFnsig20_KTOFnsig20"));
